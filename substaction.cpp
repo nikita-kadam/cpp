ce3cb3d2e6b75f24850828029ac4d0fd739b9fa5
nikita-kadam <n9689400270>
2022-03-29 11:44:44 +0530

    to add new file

---
+++ substaction.cpp
@@ -0,0 +1,24 @@
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int no1=0;
+	int no2=0;
+	int ans=0;
+	
+	cout<<"Enter first number\n";
+	cin>>no1;
+
+	cout<<"Enter second number\n";
+	cin>>no2;
+
+	ans=no1+no2;
+	cout<<"Addition is "<<ans<<"\n";
+	
+	return 0;
+}
